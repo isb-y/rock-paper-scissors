bafbba54875cdc1f8d6fde3105441ed01b9eea4b
Ibrahim Yigci <ibrahim.yigci@gmail.com>
2023-05-30 21:57:11 +0200

    prettier

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-let playerPoints=0;
-let computerPoints=0;
+let playerPoints = 0;
+let computerPoints = 0;
 
 const myArray = ["Rock", "Paper", "Scissors"];
 
@@ -12,56 +12,46 @@ function playRound(playerSelection, computerSelection) {
   console.log("Player selection: " + playerSelection_rps);
   console.log("Computer selection: " + computerSelection);
 
-  if(playerSelection_rps === computerSelection){
+  if (playerSelection_rps === computerSelection) {
     console.log("DRAW");
-  }
-  
-  else if(playerSelection_rps === "Rock"){
-    if(computerSelection ==="Paper"){
+  } else if (playerSelection_rps === "Rock") {
+    if (computerSelection === "Paper") {
       computerPoints++;
+    } else {
+      playerPoints++;
     }
-    else{
+  } else if (playerSelection_rps === "Paper") {
+    if (computerSelection === "Scissors") {
+      computerPoints++;
+    } else {
+      playerPoints++;
+    }
+  } else if (playerSelection_rps === "Scissors") {
+    if (computerSelection === "Rock") {
+      computerPoints++;
+    } else {
       playerPoints++;
     }
   }
 
-  else if(playerSelection_rps === "Paper"){
-    if(computerSelection ==="Scissors"){
-      computerPoints++;
-    }
-    else{
-      playerPoints++;
-    }
-  }
+  return "Player points:" + playerPoints + ", computerPoints:" + computerPoints;
+}
 
-  else if(playerSelection_rps === "Scissors"){
-    if(computerSelection ==="Rock"){
-      computerPoints++;
-    }
-    else{
-      playerPoints++;
-    }
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("0,1,2 for rock, paper, scissors:", "");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
   }
+  console.log("");
+  if (playerPoints === computerPoints) {
+    return "DRAW!";
+  }
+  if (playerPoints > computerPoints) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
 
-  return("Player points:" + playerPoints + ", computerPoints:" + computerPoints);
-  }
-   
-  function game(){
-    for(let i = 0; i < 5; i++){
-      const playerSelection = prompt("0,1,2 for rock, paper, scissors:", "");
-      const computerSelection = getComputerChoice();
-      console.log(playRound(playerSelection, computerSelection));
-    }
-    console.log("")
-    if(playerPoints === computerPoints){
-      return("DRAW!");
-    }
-    if(playerPoints>computerPoints){
-      return("You win!")
-    }
-    else{
-      return"You lose!";
-    }
-  }
-
-  console.log(game());
+console.log(game());
