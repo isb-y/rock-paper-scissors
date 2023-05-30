ebb95365b07b43d5bbc282472984794697cde4c6
Ibrahim Yigci <ibrahim.yigci@gmail.com>
2023-05-30 21:04:11 +0200

    test it

--- javascript.js
+++ javascript.js
@@ -11,3 +11,5 @@ function playRound(playerSelection, computerSelection) {
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+
+  //hellooo
