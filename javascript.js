2514517720257fe263278e86a441d0a92e98e666
Ibrahim Yigci <ibrahim.yigci@gmail.com>
2023-05-30 21:29:54 +0200

    game works

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let playerPoints=0;
+let computerPoints=0;
+
 const myArray = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
@@ -5,11 +8,50 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(computerSelection);
+  let playerSelection_rps = myArray[playerSelection];
+  console.log("Player selection: " + playerSelection_rps);
+  console.log("Computer selection: " + computerSelection);
+
+  if(playerSelection_rps === computerSelection){
+    console.log("DRAW");
+  }
+  
+  else if(playerSelection_rps === "Rock"){
+    if(computerSelection ==="Paper"){
+      computerPoints++;
+    }
+    else{
+      playerPoints++;
+    }
+  }
+
+  else if(playerSelection_rps === "Paper"){
+    if(computerSelection ==="Scissors"){
+      computerPoints++;
+    }
+    else{
+      playerPoints++;
+    }
+  }
+
+  else if(playerSelection_rps === "Scissors"){
+    if(computerSelection ==="Rock"){
+      computerPoints++;
+    }
+    else{
+      playerPoints++;
+    }
+  }
+
+  return("Player points:" + playerPoints + ", computerPoints:" + computerPoints);
   }
    
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  function game(){
+    for(let i = 0; i < 5; i++){
+      const playerSelection = prompt("0,1,2 for rock, paper, scissors:", "");
+      const computerSelection = getComputerChoice();
+      console.log(playRound(playerSelection, computerSelection));
+    }
+  }
 
-  //hellooo
+  game();
